784d5d59faeb19d76f68a3ae66090ee4e29a5b74
vahita <vahitakpinar50@gmail.com>
2022-12-22 00:17:21 +0300

    v2

--- src/hh/ffgf.java
+++ src/hh/ffgf.java
@@ -5,5 +5,7 @@ public class ffgf {
         System.out.println("hello");
         System.out.println("hello 2");
         System.out.println("hello 3");
+        System.out.println("cmd");
+
     }
 }
